8b4b85ca4d9cdce93fc7fcb2f9d87b8fcfcd7987
PhanDucThe <maytinh519@gmail.com>
2025-10-19 10:33:06 +0700

    create

---
+++ CongNghePhanMen/Areas/Admin/Controllers/PrescriptionManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CongNghePhanMen.Areas.Admin.Controllers
+{
+    public class PrescriptionManagementController : Controller
+    {
+        // GET: Admin/PrescriptionManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CongNghePhanMen/Areas/Admin/Controllers/ReviewManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CongNghePhanMen.Areas.Admin.Controllers
+{
+    public class ReviewManagementController : Controller
+    {
+        // GET: Admin/ReviewManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
